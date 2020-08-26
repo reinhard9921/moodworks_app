f2e086b67cfeadb81fdbe56976b594ce3270bafa
SimonevanderWalt <simone.vanderwalt@gmail.com>
2020-08-26 20:21:35 +0200

    Methods for percentages, user stats.

--- lib/Screens/overallStatsScreen_screen.dart
+++ lib/Screens/overallStatsScreen_screen.dart
@@ -1,5 +1,11 @@
-
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import '';
+import 'package:moodworksapp/Classes/User.dart';
+import 'package:moodworksapp/Share/loading.dart';
+import 'dart:io';
 
 class OverallStats_screen extends StatelessWidget {
   @override
@@ -50,4 +56,133 @@ class OverallStats_screen extends StatelessWidget {
       ),
     );
   }
-}
+
+  Future<double> getAngry(int userID) async {
+    var jsonMapAngry;
+    var jsonDataAngry;
+    var jsonMapTotal;
+    var jsonDataTotal;
+    var angryResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Angry?userID=${userID}');
+    var totalResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Total?userID=${userID}');
+
+    if (angryResponse.statusCode == 200 && totalResponse.statusCode == 200 ) {
+      jsonMapAngry = json.decode(angryResponse.body);
+      jsonMapTotal = json.decode(totalResponse.body);
+    } else {
+      throw Exception;
+    }
+
+    jsonDataAngry = jsonMapAngry['recordset'];
+    jsonDataTotal = jsonMapTotal['recordset'];
+    double percentage = jsonDataAngry.Total / jsonDataTotal.Total;
+    return percentage;
+  }
+
+  Future<double> getAnxious(int userID) async {
+    var jsonMapAnxious;
+    var jsonDataAnxious;
+    var jsonMapTotal;
+    var jsonDataTotal;
+    var anxiousResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Anxious?userID=${userID}');
+    var totalResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Total?userID=${userID}');
+
+    if (anxiousResponse.statusCode == 200 && totalResponse.statusCode == 200 ) {
+      jsonMapAnxious = json.decode(anxiousResponse.body);
+      jsonMapTotal = json.decode(totalResponse.body);
+    } else {
+      throw Exception;
+    }
+
+    jsonDataAnxious = jsonMapAnxious['recordset'];
+    jsonDataTotal = jsonMapTotal['recordset'];
+    double percentage = jsonDataAnxious.Total / jsonDataTotal.Total;
+    return percentage;
+  }
+
+  Future<double> getEnergetic(int userID) async {
+    var jsonMapEnergetic;
+    var jsonDataEnergetic;
+    var jsonMapTotal;
+    var jsonDataTotal;
+    var energeticResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Energetic?userID=${userID}');
+    var totalResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Total?userID=${userID}');
+
+    if (energeticResponse.statusCode == 200 && totalResponse.statusCode == 200 ) {
+      jsonMapEnergetic = json.decode(energeticResponse.body);
+      jsonMapTotal = json.decode(totalResponse.body);
+    } else {
+      throw Exception;
+    }
+
+    jsonDataEnergetic = jsonMapEnergetic['recordset'];
+    jsonDataTotal = jsonMapTotal['recordset'];
+    double percentage = jsonDataEnergetic.Total / jsonDataTotal.Total;
+    return percentage;
+  }
+
+  Future<double> getSad(int userID) async {
+    var jsonMapSad;
+    var jsonDataSad;
+    var jsonMapTotal;
+    var jsonDataTotal;
+    var sadResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Sad?userID=${userID}');
+    var totalResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Total?userID=${userID}');
+
+    if (sadResponse.statusCode == 200 && totalResponse.statusCode == 200 ) {
+      jsonMapSad = json.decode(sadResponse.body);
+      jsonMapTotal = json.decode(totalResponse.body);
+    } else {
+      throw Exception;
+    }
+
+    jsonDataSad = jsonMapSad['recordset'];
+    jsonDataTotal = jsonMapTotal['recordset'];
+    double percentage = jsonDataSad.Total / jsonDataTotal.Total;
+    return percentage;
+  }
+
+  Future<double> getHappy(int userID) async {
+    var jsonMapHappy;
+    var jsonDataHappy;
+    var jsonMapTotal;
+    var jsonDataTotal;
+    var happyResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Happy?userID=${userID}');
+    var totalResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Total?userID=${userID}');
+
+    if (happyResponse.statusCode == 200 && totalResponse.statusCode == 200 ) {
+      jsonMapHappy = json.decode(happyResponse.body);
+      jsonMapTotal = json.decode(totalResponse.body);
+    } else {
+      throw Exception;
+    }
+
+    jsonDataHappy = jsonMapHappy['recordset'];
+    jsonDataTotal = jsonMapTotal['recordset'];
+    double percentage = jsonDataHappy.Total / jsonDataTotal.Total;
+    return percentage;
+  }
+
+  Future<double> getTired(int userID) async {
+    var jsonMapTired;
+    var jsonDataTired;
+    var jsonMapTotal;
+    var jsonDataTotal;
+    var tiredResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Happy?userID=${userID}');
+    var totalResponse = await http.get('http://api.moodworx.co.za:2461/Mood_log_Stats_Total?userID=${userID}');
+
+    if (tiredResponse.statusCode == 200 && totalResponse.statusCode == 200 ) {
+      jsonMapTired = json.decode(tiredResponse.body);
+      jsonMapTotal = json.decode(totalResponse.body);
+    } else {
+      throw Exception;
+    }
+
+    jsonDataTired = jsonMapTired['recordset'];
+    jsonDataTotal = jsonMapTotal['recordset'];
+    double percentage = jsonDataTired.Total / jsonDataTotal.Total;
+    return percentage;
+  }
+
+
+  }
+
